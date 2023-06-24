38748a1574e365952d24dd878b6768e2fccef58a
Sam Chen <sam8211112222@gmail.com>
2023-06-24 12:17:25 +0800

    lab 1 assignment

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -11,10 +11,13 @@ public class Collatz {
     public static int nextNumber(int n) {
         if (n == 1) {
             return 1;
+
         } else if (n % 2 == 0) {
             return n / 2;
+
         } else {
             return 3 * n + 1;
+
         }
     }
 
